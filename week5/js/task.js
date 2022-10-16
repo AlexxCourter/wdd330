3dfa79fad2ddf600e1d1477d17ace58dc5cf1a09
AlexxCourter <alex.courter@gmail.com>
2022-10-16 00:34:22 -0400

    added ToDos list generation functionality

--- week5/js/task.js
+++ week5/js/task.js
@@ -1,3 +1,5 @@
+//code to be refactored next week with modules and localStorage functionality
+//for testing and building the ui
 const testList = [
     {
         id: "2022",
@@ -14,7 +16,9 @@ const testList = [
         content: "task 3",
         completed: false
     }
-]
+];
+
+const taskContainer = document.getElementById('tasks');
 
 function createTask() {
     //stub
@@ -29,6 +33,8 @@ function deleteTask() {
 }
 
 function renderSingleTask(taskString="") {
+    console.log(`creating task "${taskString}" in (renderSingleTask)`)
+
     const item = document.createElement("li");
     //create the objects within the task that can be interacted with
     item.innerHTML = `
@@ -36,8 +42,18 @@ function renderSingleTask(taskString="") {
     <div class='taskText'>${taskString}</div>
     <div class='removeButton'>X</div>   
     `
+
+    return item;
 }
 
 function renderTaskList(taskList) {
-    taskList.forEach(task => renderSingleTask(task.name))
+    console.log('using testList data to populate the ToDos list | (RenderTaskList)'); //debug
+
+    taskList.forEach(task => {taskContainer.appendChild(renderSingleTask(task.content))})
+
+    console.log('finished iterating. There should be 3 tasks from the testList'); //debug
+    console.log('Currently working functionality: generating tasks from a list of saved objects. Need to add eventlisteners for the checkboxes and remove buttons. Need to add listener for the addTask button. Need to add function for saving task objects to localStorage'); //debug
 }
+
+//for testing.
+renderTaskList(testList);
